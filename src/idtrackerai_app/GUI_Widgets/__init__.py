99c0ddf4c7c604d4dd8e962d34b46f0d8b2ced18
Jordi Torrents <jordi.torrentsm@gmail.com>
2022-10-10 10:24:32 +0100

    use __all__

--- src/idtrackerai_app/GUI_Widgets/__init__.py
+++ src/idtrackerai_app/GUI_Widgets/__init__.py
@@ -1,7 +1,17 @@
-from .bkg_subtractor import background_row  # noqa
-from .GUI_video_player import VideoPlayer  # noqa
-from .open_video_widget import OpenBtnWidget  # noqa
-from .QLabeledRangeSlider_widget import my_QLabeleRangeSlider  # noqa
-from .ROI_widget import ROI_Widget  # noqa
-from .setup_points_widget import SetupPointsWidget  # noqa
-from .tracking_interval_widget import TrackingIntervalWidget  # noqa
+from .bkg_subtractor import background_row
+from .GUI_video_player import VideoPlayer
+from .open_video_widget import OpenBtnWidget
+from .QLabeledRangeSlider_widget import my_QLabeleRangeSlider
+from .ROI_widget import ROI_Widget
+from .setup_points_widget import SetupPointsWidget
+from .tracking_interval_widget import TrackingIntervalWidget
+
+__all__ = [
+    "background_row",
+    "VideoPlayer",
+    "OpenBtnWidget",
+    "my_QLabeleRangeSlider",
+    "ROI_Widget",
+    "SetupPointsWidget",
+    "TrackingIntervalWidget",
+]
