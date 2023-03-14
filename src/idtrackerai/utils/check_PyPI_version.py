090e683cc08d60e6804e59f145d596cef5dea481
Jordi Torrents <jordi.torrentsm@gmail.com>
2023-03-14 18:25:52 +0100

    flake8

--- src/idtrackerai/utils/check_PyPI_version.py
+++ src/idtrackerai/utils/check_PyPI_version.py
@@ -43,7 +43,7 @@ def check_version() -> tuple[bool, str]:
 
     # TODO maybe use from html.parser import HTMLParser?
     no_yanked_versions = "\n".join(
-        filter(lambda l: "yanked" not in l, out_text.splitlines())
+        filter(lambda line: "yanked" not in line, out_text.splitlines())
     )
     versions: list[tuple[str, str]] = re.findall(
         ">idtrackerai-(.+?)(.tar.gz|-py3-none-any.whl)<", no_yanked_versions
