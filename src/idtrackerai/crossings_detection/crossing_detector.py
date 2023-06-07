f7d7c48f0caed5ec5f1e394f675c2096bb698726
Jordi Torrents <jordi.torrentsm@gmail.com>
2023-06-07 12:31:00 +0100

    do not detect anomalies while training

--- src/idtrackerai/crossings_detection/crossing_detector.py
+++ src/idtrackerai/crossings_detection/crossing_detector.py
@@ -166,8 +166,11 @@ def detect_crossings(list_of_blobs: ListOfBlobs, video: Video):
 
     if model_diverged:
         logging.warning(
-            "The model diverged. Falling back to individual-crossing discrimination by"
-            " average area model."
+            (
+                "[red]The model diverged[/] provably due to a bad segmentation. Falling"
+                " back to individual-crossing discrimination by average area model."
+            ),
+            extra={"markup": True},
         )
         for blob in eval_blobs:
             blob.is_an_individual = blob.seems_like_individual
