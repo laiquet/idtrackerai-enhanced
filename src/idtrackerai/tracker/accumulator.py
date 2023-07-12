fdeed3aa2ee35104b641836cd6c379f7f5b17fde
Jordi Torrents <jordi.torrentsm@gmail.com>
2023-07-13 01:00:26 +0200

    fix protocol3 smoke test

--- src/idtrackerai/tracker/accumulator.py
+++ src/idtrackerai/tracker/accumulator.py
@@ -211,4 +211,9 @@ def perform_one_accumulation_step(
         accumulation_manager.list_of_fragments.ratio_of_images_used_for_training
     )
 
-    video.accumulation_statistics_data.append(video.accumulation_statistics)
+    while len(video.accumulation_statistics_data) <= video.accumulation_trial:
+        video.accumulation_statistics_data.append({})
+
+    video.accumulation_statistics_data[video.accumulation_trial] = (
+        video.accumulation_statistics
+    )
