6da60890830612cac7f2769c1ec6e7eee34468fa
Jordi Torrents <jordi.torrentsm@gmail.com>
2023-07-12 22:13:04 +0200

    more similar stopping criterions

--- src/idtrackerai/crossings_detection/crossings_network.py
+++ src/idtrackerai/crossings_detection/crossings_network.py
@@ -45,67 +45,70 @@ class StopTraining:
         accuracy_validation: float,
         status: Status,
     ):
+        """Returns True when one of the conditions to stop the training is
+        satisfied, otherwise it returns False"""
         self.epochs_completed += 1
-        # check that the model did not diverged (nan loss).
+
         if self.epochs_completed > 0 and (
             np.isnan(loss_training) or np.isnan(loss_validation[-1])
         ):
             status.stop()
-            logging.info(
+            logging.error(
                 "The model diverged with loss NaN, falling back "
                 "to detecting crossings with the model area"
             )
             return True
         # check if it did not reached the epochs limit
-        if self.epochs_completed > self.num_epochs - 1:
+        if self.epochs_completed >= self.num_epochs:
             status.stop()
-            logging.info(
+            logging.warning(
                 "The number of epochs completed is larger than the number "
                 "of epochs set for training, we stop the training"
             )
             return True
-        # check that the model is not overfitting or if it reached a
-        # stable saddle (minimum)
-        if self.epochs_completed > self.epochs_before_checking_stopping_conditions:
-            current_loss = loss_validation[-1]
-            previous_loss = np.nanmean(
-                loss_validation[-self.epochs_before_checking_stopping_conditions : -1]
-            )
-            # The validation loss in the first 10 epochs could have
-            # exploded but being decreasing.
-            if np.isnan(previous_loss):
-                previous_loss = sys.float_info[0]
-            losses_difference = previous_loss - current_loss
-            # check overfitting
-            if losses_difference < 0.0:
-                self.overfitting_counter += 1
-                if self.overfitting_counter >= conf.OVERFITTING_COUNTER_THRESHOLD_DCD:
-                    status.stop()
-                    logging.info("Overfitting")
-                    return True
-            else:
-                self.overfitting_counter = 0
-            # check if the error is not decreasing much
-            if abs(
-                losses_difference
-            ) < conf.LEARNING_PERCENTAGE_DIFFERENCE_DCD * 10 ** (
-                int(np.log10(current_loss)) - 1
-            ):
+
+        if self.epochs_completed <= self.epochs_before_checking_stopping_conditions:
+            return False
+
+        # check that the model is not overfitting or if it reached
+        # a stable saddle (minimum)
+        current_loss = loss_validation[-1]
+        previous_loss = np.nanmean(
+            loss_validation[-self.epochs_before_checking_stopping_conditions : -1]
+        )
+
+        # The validation loss in the first 10 epochs could have exploded
+        # but being decreasing.
+        if np.isnan(previous_loss):
+            previous_loss = sys.float_info[0]
+        losses_difference = previous_loss - current_loss
+
+        # check overfitting
+        if losses_difference < 0.0:
+            self.overfitting_counter += 1
+            if self.overfitting_counter >= conf.OVERFITTING_COUNTER_THRESHOLD_DCD:
                 status.stop()
-                logging.info(
-                    "The losses difference is very small, we stop the training"
-                )
+                logging.info("Overfitting")
                 return True
-            # if the individual accuracies in validation are 1. for all the animals
-            if accuracy_validation == 1.0:
-                status.stop()
-                logging.info("The accuracy in validation is 100%, we stop the training")
-                return True
-            # if the validation loss is 0.
-            if previous_loss == 0.0 or current_loss == 0.0:
-                status.stop()
-                logging.info("The validation loss is 0.0, we stop the training")
-                return True
+        else:
+            self.overfitting_counter = 0
+        # check if the error is not decreasing much
+        if np.abs(losses_difference) < conf.LEARNING_PERCENTAGE_DIFFERENCE_DCD * 10 ** (
+            int(np.log10(current_loss)) - 1
+        ):
+            status.stop()
+            logging.info("The losses difference is very small, we stop the training")
+            return True
+        # if the individual accuracies in validation are 1. for all the animals
+        if accuracy_validation == 1.0:
+            status.stop()
+            logging.info("The accuracy in validation is 100%, we stop the training")
+            return True
+        # if the validation loss is 0.
+        if previous_loss == 0.0 or current_loss == 0.0:
+            status.stop()
+            logging.info("The validation loss is 0.0, we stop the training")
+            return True
 
         return False
 
