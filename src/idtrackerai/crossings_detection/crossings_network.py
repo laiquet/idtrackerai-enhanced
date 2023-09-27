fix overflow when loss==0

src/idtrackerai/crossings_detection/crossings_network.py
@@ -41,7 +41,7 @@ class StopTraining:
     def __call__(
         self,
         loss_training: float,
-        loss_validation: list,
+        loss_validation: list[float],
         accuracy_validation: float,
         status: Status,
     ):
@@ -93,9 +93,7 @@ class StopTraining:
         else:
             self.overfitting_counter = 0
         # check if the error is not decreasing much
-        if np.abs(losses_difference) < conf.LEARNING_PERCENTAGE_DIFFERENCE_DCD * 10 ** (
-            int(np.log10(current_loss)) - 1
-        ):
+        if abs(losses_difference) < conf.LEARNING_RATIO_DIFFERENCE_DCD * current_loss:
             status.stop()
             logging.info("The losses difference is very small, we stop the training")
             return True

src/idtrackerai/tracker/identity_network.py
@@ -120,9 +120,7 @@ class StopTraining:
 
         # check if the error is not decreasing much
 
-        if abs(losses_difference) < conf.LEARNING_PERCENTAGE_DIFFERENCE_IDCNN * 10 ** (
-            int(np.log10(current_loss)) - 1
-        ):
+        if abs(losses_difference) < conf.LEARNING_RATIO_DIFFERENCE_IDCNN * current_loss:
             status.stop()
             logging.info("The losses difference is very small, we stop the training")
             return True